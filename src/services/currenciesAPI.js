100784d67402793d7dec44c31485ece080c74bf8
carolcortes <caroline.ocortes@gmail.com>
2022-07-28 23:16:42 -0300

    requisito 3

---
+++ src/services/currenciesAPI.js
@@ -0,0 +1,8 @@
+const getAllCurrencies = async () => {
+  const URL = 'https://economia.awesomeapi.com.br/json/all';
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+};
+
+export default getAllCurrencies;
